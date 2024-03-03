3095446ac602c7b5db1f97fa822b9f9bc14fbc2d
Yauheni <adamovich.evginy@gmail.com>
2024-03-04 01:10:27 +0300

    update model

--- Assets/Scripts/Character/Enemy/ZombieSpawner.cs
+++ Assets/Scripts/Character/Enemy/ZombieSpawner.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Linq;
 using CrazyGames;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ZombieSpawner : MonoBehaviour, IZombieSpawner
 {
-    [SerializeField] private Transform[] spawnPositions;
+    [SerializeField] private bool isGizmos;
+    [SerializeField] private ZombieSpawnPositionConfiguration[] spawnPositions;
     [SerializeField] private ZombieSpawnConfiguration[] casualZombie;
 
     public int CurrentZombieAmount { get; private set; }
@@ -42,7 +45,7 @@ public class ZombieSpawner : MonoBehaviour, IZombieSpawner
         while (true)
         {
             yield return new WaitForSeconds(casualZombie[currentLvel].spawnInterval);
-            var maxZombie = 40;
+            var maxZombie = 60;
             CrazySDK.Instance.GetSystemInfo(systemInfo =>
             {
                 if (systemInfo.device.type == "desktop")
@@ -73,7 +76,34 @@ public class ZombieSpawner : MonoBehaviour, IZombieSpawner
         var randomZombie = zombieid[Random.Range(0, zombieid.Count)];
         randomZombie.SpawnedZombie--;
         var newZombie = Instantiate(randomZombie.enemyPrefabs[Random.Range(0, randomZombie.enemyPrefabs.Length)]);
-        newZombie.transform.position = pos.position;
+        var position = GetSpawnPosition();
+        newZombie.transform.position = position;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (isGizmos == false) return;
+        foreach (var targetSearcherConfiguration in spawnPositions)
+        {
+            if (targetSearcherConfiguration.SpawnCenter == null)
+            {
+                continue;
+            }
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(targetSearcherConfiguration.SpawnCenter.position,
+                new Vector3(targetSearcherConfiguration.RadiusX, 1, targetSearcherConfiguration.RadiusZ));
+        }
+    }
+
+    private Vector3 GetSpawnPosition()
+    {
+        var randomId = spawnPositions[Random.Range(0, spawnPositions.Length)];
+        var position = randomId.SpawnCenter.position;
+        var radiusXFix = randomId.RadiusX / 2f;
+        var radiusZFix = randomId.RadiusZ / 2f;
+        position += new Vector3(Random.Range(-radiusXFix, radiusXFix), 0, Random.Range(-radiusZFix, radiusZFix));
+        return position;
     }
 }
 
@@ -82,4 +112,12 @@ public interface IZombieSpawner
     int CurrentZombieAmount { get; }
 
     void SpawnZombie();
+}
+
+[Serializable]
+public class ZombieSpawnPositionConfiguration
+{
+    [field: SerializeField] public Transform SpawnCenter { get; private set; }
+    [field: SerializeField] public float RadiusX { get; private set; }
+    [field: SerializeField] public float RadiusZ { get; private set; }
 }
